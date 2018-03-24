a67901f65afb3250cedd3c6005f88ce7660ac36a
Pimpys <maxkyivua@gmail.com>
2018-03-24 15:04:15 +0300

    first commit

---
+++ index.php
@@ -0,0 +1,31 @@
+<?php
+/**
+ *Copyright (c)
+ *http://maxsuccess.ru/
+ *https://vk.com/pimpys
+ *https://www.facebook.com/the.web.lessons/
+ *Веб разработка на Yii2 Framework
+ * +7-978-051-57-37
+ * Кодируй так, как будто человек,
+ * поддерживающий твой код, - буйный психопат,
+ * знающий, где ты живешь.
+ * Created by PhpStorm.
+ * User: Max
+ * Date: 24.03.2018
+ * Time: 1:13
+ */
+
+?>
+
+<html lang="ru-RU">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title>Магазин ООП</title>
+    <meta name="description" content="Проект по ООП, создание магазина.">
+</head>
+<body>
+
+</body>
+</html>
